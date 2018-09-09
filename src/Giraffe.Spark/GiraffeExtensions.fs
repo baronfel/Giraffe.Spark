24f7508c2177e9384c3c2d77694e3dcdcbba5672
Chet Husk <chusk3@gmail.com>
2018-09-09 12:45:47 -0500

    initial test

--- src/Giraffe.Spark/GiraffeExtensions.fs
+++ src/Giraffe.Spark/GiraffeExtensions.fs
@@ -4,16 +4,10 @@ module ResponseWriters =
   open Microsoft.AspNetCore.Http
   open FSharp.Control.Tasks.V2
   open Microsoft.Net.Http.Headers
-  open System.Threading.Tasks
 
-  type HttpContext with
-    member this.WriteStreamAsync (s: System.IO.Stream) = task {
-      use stream = s
-      this.SetHttpHeader HeaderNames.ContentLength s.Length
-      do! stream.CopyToAsync(this.Response.Body)
-      return Some this
-    }
-
-  let setBodyFromStream (s: System.IO.Stream): HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) -> ctx.WriteStreamAsync s
-
+  let setBodyFromStream (s: #System.IO.Stream): HttpHandler = fun (next: HttpFunc) (ctx: HttpContext) -> task {
+    use stream = s
+    ctx.SetHttpHeader HeaderNames.ContentLength s.Length
+    do! stream.CopyToAsync(ctx.Response.Body)
+    return! next ctx
+  }
--- tests/Giraffe.Spark.Tests/Tests.fs
+++ tests/Giraffe.Spark.Tests/Tests.fs
@@ -1,9 +1,42 @@
 module Tests
 
 
+module WebApp =
+  open Spark
+  open Giraffe
+  open System.IO
+  open Microsoft.AspNetCore.Builder
+  open Microsoft.AspNetCore.Hosting
+  open Microsoft.Extensions.Logging
+  open Microsoft.Extensions.DependencyInjection
+  open Giraffe.Spark
+
+  let app = GET >=> route "/index" >=> sparkHtmlView "index.shade" ()
+
+  let contentRoot = Directory.GetCurrentDirectory()
+  let views = Path.Combine(contentRoot, "Views")
+  let builder =
+    WebHostBuilder()
+      .UseKestrel()
+      .UseContentRoot(contentRoot)
+      .Configure(fun c -> c.UseGiraffe(app))
+      .ConfigureServices(fun c -> c.AddSparkViewEngine(views) |> ignore)
+
 open Expecto
-open Giraffe.Spark
+open Microsoft.AspNetCore.TestHost
 
 [<Tests>]
 let tests =
-  testList "samples" [ ]
+  testList "samples" [
+    test "can get a rendered template" {
+      use host = new TestServer(WebApp.builder)
+      let call = host.CreateRequest("/index").GetAsync()
+      do Async.Sleep 2000 |> Async.RunSynchronously
+      Expect.isTrue call.IsCompleted (sprintf "Call errored with %A" call.Exception)
+      let req = call.Result
+      Expect.equal req.StatusCode System.Net.HttpStatusCode.OK "should get ok"
+      let body = req.Content.ReadAsStringAsync().Result
+      Expect.equal body "Hello from Spark View Engine!" "should render view"
+    }
+  ]
+
